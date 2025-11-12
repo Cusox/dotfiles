ecc36739394e1e1e1d9bfade46f6d54a661e670c
Cusox <cusoxlee@gmail.com>
2025-11-12 23:54:18 +0800

    refactor: `treesitter` plugin for installing parsers

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,43 +5,74 @@ return {
 		branch = "main",
 		build = ":TSUpdate",
 		config = function()
-			local ensureInstalled = {
+			local ts = vim.treesitter
+
+			local parsers = {
 				"bash",
 				"c",
+				"cmake",
 				"cpp",
+				"css",
 				"cuda",
+				"diff",
 				"dockerfile",
 				"git_config",
+				"git_rebase",
+				"gitcommit",
+				"gitignore",
 				"go",
 				"gomod",
+				"gosum",
 				"gotmpl",
 				"html",
+				"java",
+				"javascript",
+				"json",
 				"latex",
 				"lua",
 				"make",
 				"markdown",
 				"markdown_inline",
+				"ninja",
+				"regex",
 				"rust",
+				"scss",
+				"toml",
+				"xml",
+				"yaml",
 			}
-			local alreadyInstalled = require("nvim-treesitter.config").get_installed()
-			local parsersToInstall = vim.iter(ensureInstalled)
-				:filter(function(parser)
-					return not vim.tbl_contains(alreadyInstalled, parser)
-				end)
-				:totable()
-			require("nvim-treesitter").install(parsersToInstall)
+			local parsers_installed = require("nvim-treesitter").get_installed()
+			local filetypes = vim.iter(parsers):map(ts.language.get_filetypes):flatten():fold({}, function(tbl, v)
+				tbl[v] = vim.tbl_contains(parsers_installed, v)
+				return tbl
+			end)
+
+			local ts_enable = function(buffer, lang)
+				local ok, hl = pcall(vim.query.get, lang, "highlights")
+				if ok and hl then
+					vim.treesitter.start(buffer, lang)
+				end
+			end
 
 			vim.api.nvim_create_autocmd("FileType", {
-				callback = function(args)
-					local filetype = args.match
-					local lang = vim.treesitter.language.get_lang(filetype)
-					if vim.treesitter.language.add(lang) then
-						vim.wo.foldlevel = 99
-						vim.wo.foldmethod = "expr"
-						vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-						vim.treesitter.start()
+				desc = "enable treesitter",
+				callback = function(event)
+					local ft = event.match
+					local available = filetypes[ft]
+					if available == nil then
+						return
 					end
+
+					local lang = ts.language.get_lang(ft)
+					if available then
+						ts_enable(event.buf, lang)
+						return
+					end
+
+					require("nvim-treesitter").install(lang):await(function()
+						filetypes[ft] = true
+						ts_enable(event.buf, lang)
+					end)
 				end,
 			})
 		end,
