0503633a3549c78f03aad6d285dddcaf42752f08
Cusox <cusoxlee@gmail.com>
2026-03-10 11:00:46 +0800

    feat: trace `pwsh` config

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+# scoop-search
+. ([ScriptBlock]::Create((& scoop-search --hook | Out-String)))
+
+# mise
+. ([ScriptBlock]::Create((& mise activate pwsh | Out-String)))
+
+# atuin
+. ([ScriptBlock]::Create((& atuin init powershell | Out-String)))
+
+# starship
+. ([ScriptBlock]::Create((& starship init powershell | Out-String)))
