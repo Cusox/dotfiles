b47e6fcf09edddbb9aa998fcdcb634d211d925e6
Cusox <cusoxlee@gmail.com>
2026-03-11 14:51:54 +0800

    feat: replace `diffview.nvim` with `codediff.nvim`

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"esmuellert/codediff.nvim",
+		cmd = "CodeDiff",
+	},
+	{
 		"NeogitOrg/neogit",
 		cmd = "Neogit",
 		dependencies = {
@@ -8,21 +17,6 @@ return {
 		},
 		keys = {
 			{ "<Leader>g", "<CMD>Neogit<CR>", desc = "Open Neogit" },
-		},
-		opts = {},
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
-	{
-		"sindrets/diffview.nvim",
-		cmd = "DiffviewOpen",
-		keys = {
-			{ "<Leader>dv", "<CMD>DiffviewOpen<CR>", desc = "Open Diffview" },
-			{ "<Leader>dh", "<CMD>DiffviewFileHistory<CR>", desc = "Open Diffview File History" },
-			{ "<Leader>dc", "<CMD>DiffviewClose<CR>", desc = "Close Diffview" },
 		},
 		opts = {},
 	},
