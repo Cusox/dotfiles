a8ad76a7c723a2f43d12f7a02731d25536d4c678
Cusox <cusoxlee@gmail.com>
2026-03-28 17:07:39 +0800

    remove `copilot-lsp`

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -8,31 +8,6 @@ return {
 		opts = {},
 	},
 	{
-		"copilotlsp-nvim/copilot-lsp",
-		init = function()
-			vim.g.copilot_nes_debounce = 500
-			vim.lsp.enable("copilot_ls")
-			vim.keymap.set("n", "<tab>", function()
-				local bufnr = vim.api.nvim_get_current_buf()
-				local state = vim.b[bufnr].nes_state
-				if state then
-					local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-						or (
-							require("copilot-lsp.nes").apply_pending_nes()
-							and require("copilot-lsp.nes").walk_cursor_end_edit()
-						)
-					return nil
-				else
-					return "<C-i>"
-				end
-			end, { desc = "Accept Copilot NES suggestion", expr = true })
-			vim.keymap.set("n", "<esc>", function()
-				if not require("copilot-lsp.nes").clear() then
-				end
-			end, { desc = "Clear Copilot suggestion or fallback" })
-		end,
-	},
-	{
 		"saghen/blink.cmp",
 		version = "1.*",
 		event = { "InsertEnter", "CmdlineEnter" },
