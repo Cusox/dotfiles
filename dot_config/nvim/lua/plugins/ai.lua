deacf1b86ae4cffd54ef26913bef613afc2baf27
Cusox <cusoxlee@gmail.com>
2026-03-28 17:25:39 +0800

    feat: add `pi.nvim`

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+	{
+		"pablopunk/pi.nvim",
+		keys = {
+			{
+				"<leader>ai",
+				"<cmd>PiAsk<cr>",
+				desc = "Ask Pi",
+			},
+			{
+				"<leader>av",
+				"<cmd>PiAskSelection<cr>",
+				desc = "Ask Pi (Selection)",
+				mode = "v",
+			},
+		},
+		opts = {},
+	},
+}
