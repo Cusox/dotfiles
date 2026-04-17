34a47c96e333a56aac0a0aef14f6a682ff81354f
Cusox <cusoxlee@gmail.com>
2026-04-17 13:22:48 +0800

    feat: add keymap for restart neovim

--- dot_config/nvim/lua/keymap.lua
+++ dot_config/nvim/lua/keymap.lua
@@ -76,3 +76,10 @@ vim.keymap.set({ "n", "x", "o" }, "<A-i>", function()
 		vim.lsp.buf.selection_range(-vim.v.count1)
 	end
 end, { desc = "Select child (inner) node" })
+
+--- Restart Neovim ---
+vim.keymap.set("n", "<leader>r", function()
+	local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+	vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+	vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart Neovim" })
